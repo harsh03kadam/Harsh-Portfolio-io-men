46369d2cb4b9f785f5846da894e6a8f420b234b0
harsh0kadam <harsh0kadam@gmail.com>
2024-05-06 20:21:08 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,543 @@
+//Frontend //
+import Html  from "../images/Frontend/icons8-html-48.png"
+import Css from "../images/Frontend/icons8-css-48.png"
+import JavaScript from "../images/Frontend/icons8-javascript-48.png"
+import TypeScript from "../images/Frontend/icons8-typescript-48.png"
+import React from "../images/Frontend/react.png"
+import Next from "../images/Frontend/nextjs.svg"
+import Bootstrap from "../images/Frontend/icons8-bootstrap-48.png"
+import Material  from "../images/Frontend/material-ui.svg"
+import Chakra from "../images/Frontend/icons8-chakra-ui-48.png"
+import Tailwind from "../images/Frontend/tailwind-css.svg"
+import Shahcn from "../images/Frontend/Shadcn.jpg"
+
+// Backend //
+import Node from "../images/Backend/node-js.svg"
+import Express from "../images/Backend/Express.png"
+import Strapi from "../images/Backend/strapi.svg"
+import AppWrite from "../images/Backend/Appwirte.png"
+import Sanity from "../images/Backend/Sanity.io.png"
+import Redis from "../images/Backend/Redis.png"
+import Graphql from "../images/Backend/Graphql.jpg"
+import Nginx from "../images/Backend/nginx.png"
+
+// DataBase //
+import mongodb from "../images/Database/mongodb-original.svg"
+import mysql from "../images/Database/mysql-original-wordmark.svg"
+import postgresql from "../images/Database/postgresql.svg"
+import firebase from "../images/Database/file-type-firebase.svg"
+import Supabase from "../images/Database/Supabase.jpg"
+
+// tools //
+
+
+import git from "../images/Tools/icons8-git-48.png"
+import github  from "../images/Tools/icons8-github-50.png"
+import docker from "../images/Tools/icons8-docker-48.png"
+import  vscode from "../images/Tools/icons8-visual-studio-code-48.png"
+import Postman from "../images/Tools/postman.png"
+import Netlify from "../images/Tools/Netlify.png"
+import  Vercel from "../images/Tools/Vercel.png"
+
+// Experience //
+
+
+import flipr   from "../images/Experience/flipr.jpeg"
+import  Rudraksha from "../images/Experience/Rudraksha.jpeg"
+
+
+// Education //
+
+import  sage  from "../images/Education/Sage.jpg"
+import unique  from "../images/Education/unique.jpg"
+
+
+// Projects //
+
+import ai  from "../images/Projact/Ai-sass.png"
+import amzone  from "../images/Projact/amzone.png"
+import  banking from "../images/Projact/banking.png"
+import hotal   from "../images/Projact/hotal.png"
+import  lingo from "../images/Projact/lingo.png"
+import  snap from "../images/Projact/snap.png"
+import  soical from "../images/Projact/soical.media.png"
+import zoom  from "../images/Projact/Zoom.png"
+import  chat from "../images/Projact/chat.png"
+
+export const Bio = {
+  name: "Harsh Kadam",
+  roles: [
+    "Full Stack Developer",
+    "Frontend.js Developer",
+    "Backend Developer",
+    "UI-UX Developer",
+    "Programmer",
+  ],
+  description:
+    "I'm a full stack developer that thrives in both front-end and back-end work, creating user interfaces that are smooth and reliable at the same time. I'm skilled at converting customer needs into scalable and effective solutions, and I have experience with a variety of databases, frameworks, and programming languages. I also thrive in dynamic contexts.",
+  github: "https://github.com/harsh03kadam",
+  resume:
+    "https://app.luminpdf.com/viewer/6638ed627f0f7ea04dc18bda",
+  linkedin: "https://www.linkedin.com/in/harsh0kadam/",
+  twitter: "https://twitter.com/_harshit_333",
+  insta: "https://www.instagram.com/_harshit_333/",
+  facebook: "https://www.facebook.com/profile.php?id=100086492767966",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "Html",
+        image:Html,
+      },
+      {
+        name: "Css",
+        image:Css,
+      },
+      {
+        name: "JavaScript",
+        image:JavaScript,
+      },
+      {
+        name: "TypeScript",
+        image:TypeScript,
+      },
+      {
+        name: "React.js",
+        image:React,
+      },
+      {
+        name: "Next.js",
+        image:Next,
+      },
+      {
+        name: "Bootstrap",
+        image:Bootstrap,
+      },
+      {
+        name: "Material-UI",
+        image:Material,
+      },
+      {
+        name: "Chakra-UI",
+        image:Chakra,
+      },
+      {
+        name: "Tailwind.Css",
+        image:Tailwind,
+      },
+      {
+        name: "Shad'cn",
+        image:Shahcn,
+      },
+   
+     
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+     
+      {
+        name: "Node.js",
+        image:Node ,
+      },
+      {
+        name: "Express.js",
+        image: Express,
+      },
+      {
+        name: "Strapi",
+        image:Strapi ,
+      },
+      {
+        name: "AppWrite",
+        image: AppWrite,
+      },
+      {
+        name: "Sanity.io",
+        image:Sanity ,
+      },
+      {
+        name: "Redis",
+        image: Redis,
+      },
+      {
+        name: "Graphql",
+        image: Graphql ,
+      },
+      {
+        name: "Nginx",
+        image: Nginx,
+      },
+
+    ],
+  },
+  {
+    title: "DataBase",
+    skills: [
+      {
+        name: "MongoDb",
+        image:mongodb,
+          
+      },
+      {
+        name: "MySql",
+        image: mysql,
+          
+      },
+      {
+        name: "Postgresql",
+        image:postgresql,
+          
+      },
+      {
+        name: "Firebase",
+        image:firebase,
+          
+      },
+      {
+        name: "Supabase",
+        image:Supabase,
+          
+      },
+ 
+   
+    
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+
+      {
+        name: "Git",
+        image:git,
+      },
+      {
+        name: "github",
+        image:github,
+      },
+      {
+        name: "Docker",
+        image:docker,
+      },
+      {
+        name: "VsCode",
+        image:vscode,
+      },
+      {
+        name: "Postman",
+        image:Postman,
+      },
+      {
+        name: "Netlify",
+        image:Netlify,
+      },
+      {
+        name: "Vercel",
+        image:Vercel,
+      },
+   
+  
+    ],
+  },
+];
+
+export const experiences = [
+ 
+ 
+  
+  {
+    id: 0,
+    img: flipr ,
+    role: "Fullstack Externship",
+    company: "Flipr",
+    date: "June 2023 - July 2023",
+    desc: "Built an Ai Sass imaginify full stack web app used Clerk and deployed on Vercel. I was the top performer in the program.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Clerk",
+      "React,Hooks",
+      "MongoDB",
+    ],
+    doc: "",
+  },
+  {
+    id: 1,
+    img:Rudraksha ,
+    role: "Frontend Developer Intern",
+    company: "Rudraksha Welffare Foundation",
+    date: "June 2023 - Oct 2023",
+    desc: "• Built Admin Dhasbord - Soical-media App, with MVVM Architecture using React.js and  integrating Firebase SDK. Created One to One video call integration with Socket.IO and Firebase cloud functions and integrated with Retrofit. Created Low Level Design and converted Figma design to  code.",
+    skills: [
+      "JavaScript",
+      "React.js",
+      "Bootstrap",
+      "Material UI",
+      "Node Js",
+      
+      "Firebase",
+      
+    ],
+    doc: "",
+  },
+
+
+];
+
+export const education = [
+  {
+    id: 0,
+    img: sage ,
+    school: "Sage University Indore [M.P]",
+    date: "Oct 2019 - Sep 2023",
+    grade: "7.25 CGPA",
+    desc: "I am currently pursuing a Master's degree in Information technology  Engineering at Sage University ,Indore Madhya Pradesh. I have Running My semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others I am learning and working on exciting projects with a team of talented developers",
+    degree: "MMasterof Technology - MTech, Information technology",
+  },
+  {
+    id: 1,
+    img: sage,
+    school: "Sage University Indore [M.P]",
+    date: "Oct 2019 - Sep 2023",
+    grade: "7.25 CGPA",
+    desc: "I am  pursuing a Bachelor's degree in Computer Science and Engineering at Sage University Indore Madhya Pradesh, .  I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. , where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 2,
+    img: unique ,
+    school: "Unique High Secondary school Ambedkar Nagar Mhow (M.P)",
+    date: "Apr 2018 - Apr 2019",
+    grade: "75%",
+    desc: "I completed my class 12 high school education at Unoque School, Ambedkar Nagar Mhow, where I studied Science with Computer Science.",
+    degree: "ISC(XII), Science with Computer",
+  },
+  {
+    id: 3,
+    img:unique ,
+    school: "Unique  High Secondary School Mhow (M.P)",
+    date: "Apr 2014 - Apr 2015",
+    grade: "60%",
+    desc: "I completed my class 10 education at Unique School, Ambedkar Nagar Mhow, where I studied Science with Computer Application.",
+    degree: "ICSC(X), Science with Computer",
+  },
+];
+
+export const projects = [
+
+
+  {
+    id: 0,
+    title: "Banking & finance management[Horizon]",
+    date: "Jan 2024 - Fab 2024",
+    description:
+      " Built with Next.js, Horizon is a financial SaaS platform that connects to multiple bank accounts, displays transactions in real-time, allows users to transfer money to other platform users, and manages their finances altogether.",
+    image: banking,
+    tags: [
+      "Next.js",
+      "Appwrite",
+      "Plain",
+      "Dwolla",
+      "React Hook Form",
+      "Zod",
+      "Tailwind.css",
+      "Chart",
+      "ShadCn",
+
+    ],
+    category: "web app",
+    github: "https://github.com/harsh03kadam/Banking-Finance-management-Horizon-",
+    webapp: "https://banking-jet.vercel.app/",
+  },
+  {
+    id: 1,
+    title: "Zoom-to-Doom",
+    date: "Jan 2024 - Fab 2024",
+    description:
+      "Built with the latest Next.js and TypeScript, this project replicates Zoom, a widely used video conferencing tool. It enables users to securely log in, create meetings and access various meeting functionalities such as recording, screen sharing, and managing participants",
+    image: zoom,
+    tags: [
+      "TypeScript",
+      "Next.js",
+      "Node Js",
+      "Express Js",
+      "Clerk",
+      "getstream",
+      "Shadcn",
+      "Tailwind.css",
+    ],
+    category: "web app",
+    github: "https://github.com/harsh03kadam/zoom-clone",
+    webapp: "https://zoom-clone-seven-rho.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Ai-Sass-Imaginify",
+    date: "Jan 2024 - Fab 2024",
+    description:
+      "Build an AI image SaaS platform that excels in image processing capabilities, integrates a secure payment infrastructure, offers advanced image search functionalities, and supports multiple AI features, including image restoration, recoloring, object removal, generative filling, and background removal. This project can be a guide for your next AI image tool",
+    image: ai ,
+      
+    tags: [
+      "Next.js",
+      "TypeScript",
+      "MongoDB",
+      "Clerk",
+      "Cloudinary",
+      "Stripe",
+      "Shadcn",
+      "TailwindCss",
+      
+
+    ],
+    category: "web app",
+    github: "https://github.com/harsh03kadam/Ai-Sass-Imaginif",
+    webapp: "https://imaginify-new.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Hotel-Management-webapp",
+    date: "Jan 2023 - Fab 2023",
+    description:
+      "The platform provides secure user access, community image showcase with pagination, advanced search, image restoration, color replacement, seamless area filling, precise object removal, downloadable and shareable transformed images with detailed information, transformation control, credit earning/purchasing, personalized profiles with credit info and Stripe integration, ensuring seamless cross-device experience with responsive UI/UX design.",
+    image: hotal,
+     
+    tags: [
+      "Next.js",
+      "TypeScript",
+      "MongoDB",
+      "Clerk",
+      "Cloudinary",
+      "Stripe",
+      "Shadcn",
+      "TailwindCss",
+      
+
+    ],
+    category: "web app",
+    github: "https://github.com/harsh03kadam/Hotel--Management-web-app",
+    webapp: "https://hotel-management-iota.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Snapgram",
+    date: "Jan 2023 - Fab 2023",
+    description:
+      "Explore social media with this user-friendly platform that has a nice look and lots of features. Easily create and explore posts, and enjoy a strong authentication system and quick data fetching using React Query for a smooth user experience.",
+    image: soical,
+     
+    tags: [
+      "React.js",
+      "TypeScript",
+      "React Query",
+      "Appwrite",
+      "Shadcn",
+      "TailwindCss",
+
+
+    ],
+    category: "web app",
+    github: "https://github.com/harsh03kadam/Snapgram-Social-Media-web-app",
+    webapp: "https://snapgram-sand.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Amzone-Clone",
+    date: "Jan 2024 - Fab 2024",
+    description:
+      "The platform provides secure user access, community image showcase with pagination, advanced search, image restoration, color replacement, seamless area filling, precise object removal, downloadable and shareable transformed images with detailed information, transformation control, credit earning/purchasing, personalized profiles with credit info and Stripe integration, ensuring seamless cross-device experience with responsive UI/UX design.",
+    image:amzone ,
+      
+    tags: [
+      "Next.js",
+      "TypeScript",
+      "MongoDB",
+      "Clerk",
+      "Cloudinary",
+      "Stripe",
+      "Shadcn",
+      "TailwindCss",
+      
+
+    ],
+    category: "web app",
+    github: "https://github.com/harsh03kadam/Amazon-clone",
+    webapp: "https://amazon-clone-nu-khaki.vercel.app/",
+  },
+
+  {
+    id: 1,
+    title: "Duolingo-clone",
+    date: "Jan 2024 - Fab 2024",
+    description:
+      "The Duolingo app is a popular language-learning platform that offers courses in numerous languages. It uses a gamified approach to make learning languages engaging and fun. Users can practice reading, writing, listening, and speaking skills through a variety of exercises, including matching, translation, listening comprehension, and speaking exercises. The app tracks progress and awards points and virtual currency for completing lessons and achieving goals. Duolingo offers both free and premium subscription options, with the premium version providing additional features such as ad-free learning, access, and progress tracking.",
+    image:lingo ,
+      
+    tags: [
+      "Next.js",
+      "TypeScript",
+      "MongoDB",
+      "Shadcn",
+      "TailwindCss",
+
+    ],
+    category: "web app",
+    github: "https://next14-duolingo-clone.vercel.app/",
+    webapp: "https://next14-duolingo-clone.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "SnapChat-clone",
+    date: "Jan 2023 - Fab 2023",
+    description:
+      "Snap is a social media platform where users can share photos and short videos with their friends. Winning a Snap clone in Next.js involves building a responsive UI/UX and implementing features like user authentication, photo/video uploading, real-time messaging, and explore/feed functionalities within a Next.js framework..",
+    image: snap,
+     
+    tags: [
+      "Next.js",
+      "Next Auth",
+      "Express.js",
+      "Mongodb",
+      "TailwindCss",
+
+
+    ],
+    category: "web app",
+    github: "https://github.com/harsh03kadam/SnapChat-App",
+    webapp: "https://snapchat-flax.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Chat-App",
+    date: "Jan 2023 - Fab 2023",
+    description:
+      "A chat app built with the MERN stack allows real-time messaging with MongoDB for data storage, Express.js for server-side logic, React.js for the frontend interface, and Node.js for backend functionality. To use, users can register/login, access chatrooms, send/receive messages, and utilize features like notifications and user authentication for a seamless communication experience.",
+    image: chat,
+     
+    tags: [
+      "React.js",
+      "JavaScript",
+      "Express.js",
+      "Zustand",
+      "MongoDB",
+      "TailwindCss",
+
+    ],
+    category: "web app",
+    github: "https://github.com/harsh03kadam/Chat-App",
+    webapp: "https://mern-chat-app-h24i.onrender.com/",
+  },
+ 
+];
+
